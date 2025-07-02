2bdbec7110aa8800b9228dbc796fcf78889ddb04
Mohd ADIL <shaikhmdadil191@gmail.com>
2025-07-02 11:44:38 +0530

    Update backend for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -44,9 +44,4 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  
-
-});
+module.exports = app;
